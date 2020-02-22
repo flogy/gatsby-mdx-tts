9d647450654d9e6a8502b069e576bb75531c526c
Florian Gyger <gygy1992@hotmail.com>
2020-02-22 18:41:51 +0100

    commit accidentally ignored file

---
+++ src/internals/utils/getSsmlFromMdxAst.ts
@@ -0,0 +1,26 @@
+import { Node } from "unist";
+
+const defaultBreakTag = "<break time='1s'/>";
+
+const mdastToSsmlString = (node: Node) => {
+  let ssmlString = "";
+  if (node.type !== "jsx") {
+    ssmlString = ssmlString.concat(
+      ((node && node.value && node.value) as string) || ""
+    );
+  }
+  const children: any = node.children;
+  if (children) {
+    ssmlString = ssmlString.concat(children.map(mdastToSsmlString).join(""));
+  }
+  if (node.type === "paragraph" || node.type === "heading") {
+    ssmlString = ssmlString.concat(defaultBreakTag);
+  }
+  return ssmlString;
+};
+
+const getSsmlFromMdxAst = (mdAst: Node) => {
+  return mdastToSsmlString(mdAst);
+};
+
+export default getSsmlFromMdxAst;
