5548af983c7cc8c571ddc18cabf1b91c1e8d0980
Florian Gyger <gygy1992@hotmail.com>
2020-03-03 10:48:25 +0100

    extracted function to mark child texts

--- src/internals/components/WordMarker.tsx
+++ src/internals/components/WordMarker.tsx
@@ -22,59 +22,63 @@ const deepMap = (children: React.ReactNode, map: MapFunction): React.ReactNode =
   });
 };
 
+export const markChildText = (children: React.ReactNode, markedWordIndex: number): React.ReactNode => {
+  const newChildren = [];
+
+  let currentIndex = 0;
+  const markWordIfRequired: MapFunction = child => {
+    if (!child) {
+      return child;
+    }
+
+    const isText = typeof child === "string";
+    if (isText) {
+      const whitespaceOrNewlineRegex = /[\s\n]/;
+      const words = (child as string).split(whitespaceOrNewlineRegex);
+
+      const doesChildContainHighlightedWord =
+          markedWordIndex >= currentIndex &&
+          markedWordIndex < currentIndex + words.length;
+
+      if (!doesChildContainHighlightedWord) {
+        currentIndex += words.length;
+        return child;
+      }
+
+      const wordIndexInsideChild = markedWordIndex - currentIndex;
+      const textBeforeHighlightedWord = words
+          .slice(0, wordIndexInsideChild)
+          .join(" ");
+      const textAfterHighlightedWord = words
+          .slice(wordIndexInsideChild + 1)
+          .join(" ");
+      const highlightedWordComponent = React.createElement(
+          "mark",
+          null,
+          words[wordIndexInsideChild]
+      );
+
+      currentIndex += words.length;
+      return [
+        `${textBeforeHighlightedWord} `,
+        highlightedWordComponent,
+        ` ${textAfterHighlightedWord}`
+      ];
+    }
+    return child;
+  };
+  newChildren.push(deepMap(children, markWordIfRequired));
+
+  return newChildren;
+};
+
 const WordMarker: React.FunctionComponent<WordMarkerProps> = props => {
   const [manipulatedChildren, setManipulatedChildren] = React.useState<
     React.ReactNode
   >(props.children);
 
   React.useEffect(() => {
-    const newChildren = [];
-
-    let currentIndex = 0;
-    const markWordIfRequired: MapFunction = child => {
-      if (!child) {
-        return child;
-      }
-
-      const isText = typeof child === "string";
-      if (isText) {
-        const whitespaceOrNewlineRegex = /[\s\n]/;
-        const words = (child as string).split(whitespaceOrNewlineRegex);
-
-        const doesChildContainHighlightedWord =
-          props.markedWordIndex >= currentIndex &&
-          props.markedWordIndex < currentIndex + words.length;
-
-        if (!doesChildContainHighlightedWord) {
-          currentIndex += words.length;
-          return child;
-        }
-
-        const wordIndexInsideChild = props.markedWordIndex - currentIndex;
-        const textBeforeHighlightedWord = words
-          .slice(0, wordIndexInsideChild)
-          .join(" ");
-        const textAfterHighlightedWord = words
-          .slice(wordIndexInsideChild + 1)
-          .join(" ");
-        const highlightedWordComponent = React.createElement(
-          "mark",
-          null,
-          words[wordIndexInsideChild]
-        );
-
-        currentIndex += words.length;
-        return [
-          `${textBeforeHighlightedWord} `,
-          highlightedWordComponent,
-          ` ${textAfterHighlightedWord}`
-        ];
-      }
-      return child;
-    };
-    newChildren.push(deepMap(props.children, markWordIfRequired));
-
-    setManipulatedChildren(newChildren);
+    setManipulatedChildren(markChildText(props.children, props.markedWordIndex));
   }, [props.children, props.markedWordIndex]);
 
   return <>{manipulatedChildren}</>;
